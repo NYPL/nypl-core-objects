effc3a06e8fde35f91f37202c8f7d8abbf6c10de
nodanaonlyzuul <beholdthepanda@gmail.com>
2017-06-30 17:32:37 -0400

    POC that we can JSON.parse() json-ld files
    We'll probably want to make this mockable
    so we can not hit github, and hit our ./test/resources directory
    in npm test mode

--- nypl-core-objects.js
+++ nypl-core-objects.js
@@ -1,12 +1,14 @@
-const request = require('request')
-
-
+const request = require('sync-request')
 
 module.exports = (maptype) => {
   if (maptype === 'from-recap-customer-codes') {
+    
+    let locationsJsonLD = JSON.parse(request('GET', 'https://raw.githubusercontent.com/NYPL/nypl-core/master/vocabularies/json-ld/locations.json').getBody())
+    let recapCustomerCodes = JSON.parse(request('GET', 'https://raw.githubusercontent.com/NYPL/nypl-core/master/vocabularies/json-ld/recapCustomerCodes.json').getBody())
+
     // Bring down latest files
-    //   https://raw.githubusercontent.com/NYPL/nypl-core/master/vocabularies/json-ld/locations.json
-    //   https://raw.githubusercontent.com/NYPL/nypl-core/master/vocabularies/json-ld/recapCustomerCodes.json
+    //
+    //
     // create the datastructure
 
     // return the datastructure
