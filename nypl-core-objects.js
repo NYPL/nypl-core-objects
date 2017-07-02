efba80c62b1e0eb8ebd6421a6ff1c0a1695ef9e0
nodanaonlyzuul <beholdthepanda@gmail.com>
2017-07-02 07:13:57 -0400

    Rename modules

--- nypl-core-objects.js
+++ nypl-core-objects.js
@@ -1,7 +1,7 @@
-const FromReCapCustomerCodeFactory = require('./lib/from_recap_customer_code_factory.js')
+const ByRecapCustomerCodeFactory = require('./lib/by_recap_customer_code_factory.js')
 
 module.exports = (maptype) => {
-  if (maptype === 'from-recap-customer-codes') {
-    return FromReCapCustomerCodeFactory.mapping()
+  if (maptype === 'by-recap-customer-codes') {
+    return ByRecapCustomerCodeFactory.mapping()
   }
 }
--- test/nypl-core-objects.test.js
+++ test/nypl-core-objects.test.js
@@ -4,22 +4,22 @@ const fs = require('fs')
 const path = require('path')
 
 function takeThisPartyOffline () {
-  let FromReCapCustomerCodeFactory = require('../lib/from_recap_customer_code_factory.js')
+  let ByRecapCustomerCodeFactory = require('../lib/by_recap_customer_code_factory.js')
   let mockedRecap = sinon.stub().returns(JSON.parse(fs.readFileSync(path.join(__dirname, './resources/recapCustomerCodes.json'))))
   let mockedSierra = sinon.stub().returns(JSON.parse(fs.readFileSync(path.join(__dirname, './resources/locations.json'))))
-  FromReCapCustomerCodeFactory._getSierraJsonLD = mockedSierra
-  FromReCapCustomerCodeFactory._getRecapJsonLD = mockedRecap
+  ByRecapCustomerCodeFactory._getSierraJsonLD = mockedSierra
+  ByRecapCustomerCodeFactory._getRecapJsonLD = mockedRecap
 }
 
 describe('nypl-core-objects', function () {
   before(function () {
     takeThisPartyOffline()
-    this.objectMaps = require('../nypl-core-objects')('from-recap-customer-codes')
+    this.byRecapCustomerCode = require('../nypl-core-objects')('by-recap-customer-codes')
   })
 
   it('exports a simpleObject', function (done) {
-    expect(this.objectMaps).to.not.equal(undefined)
-    expect(this.objectMaps).to.be.a('object')
+    expect(this.byRecapCustomerCode).to.not.equal(undefined)
+    expect(this.byRecapCustomerCode).to.be.a('object')
     done()
   })
 
