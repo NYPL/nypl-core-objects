142ec9d19e84f86ca732497c39790d524b3bbc7a
nodanaonlyzuul <beholdthepanda@gmail.com>
2017-07-06 07:02:15 -0400

    Add tests for forcetoFlatArray() and handle passing undefined

---
+++ test/json-ld-parse-utils.test.js
@@ -0,0 +1,12 @@
+let expect = require('chai').expect
+let forcetoFlatArray = require('../lib/jsonld-parse-utils').forcetoFlatArray
+
+describe('forcetoFlatArray', function () {
+  it('takes anything and turns it into a flat array', function () {
+    expect(forcetoFlatArray(1)).to.eql([1])
+    expect(forcetoFlatArray([1])).to.eql([1])
+    expect(forcetoFlatArray([1, [2], [3, 4, [5]]])).to.eql([1, 2, 3, 4, 5])
+    expect(forcetoFlatArray()).to.eql([])
+    expect(forcetoFlatArray(false)).to.eql([false])
+  })
+})
